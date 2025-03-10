ebb27a860036976d6ca38934e99540078be5923a
MinChatr <min_etc@hotmail.com>
2025-03-10 13:50:46 +0700

    Lab 6 Task 9 - Create Pagination View and Using Computed - 2

--- 713-frontend/src/router/index.ts
+++ 713-frontend/src/router/index.ts
@@ -1,5 +1,8 @@
+import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from "@/views/event/LayoutView.vue"
+import EventRegisterView from '@/views/event/RegisterView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import EventDetailView from '../views/EventDetailView.vue'
+import EventDetailView from '../views/event/DetailView.vue'
 import EventListView from '../views/EventListView.vue'
 
 const router = createRouter({
@@ -14,10 +17,30 @@ const router = createRouter({
       }),
     },
     {
-        path: '/event/:id',
-        name: 'event-detail-view',
-        component: EventDetailView,
-        props: true,
+      path: '/event/:id',
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true,
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true,
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true,
+        },
+      ],
     },
     {
       path: '/about',
